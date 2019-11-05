113f8fbb23f821421e1495c43ed5dc7ddbe3a080
pengxueyou <pxy@gato.com.cn>
2019-11-05 09:43:41 +0800

    账号管理的API

--- src/pages/user/http/ajax.js
+++ src/pages/user/http/ajax.js
@@ -1,9 +1,10 @@
 import axios from "@/utils/Request";
 import RestApi from "@/utils/RestApi";
+// import Axios from "axios";
 // import store from "@/store/store.js";
 
 let userUrl = RestApi.api.userUrl;
-
+let accountApi = RestApi.api.accountApi;
 export function getAccout(params) {
   let { getAccoutUrl: url } = userUrl;
   return axios({
@@ -58,6 +59,66 @@ export function updateUserStatus(data) {
   let { updateUserStatusUrl: url } = userUrl;
   return axios({
     method: "PUT",
+    url,
+    data
+  });
+}
+/** ***************************账号管理****************************** */
+// add
+export function addAccountApi(data) {
+  let url = accountApi.addAccountApi;
+  return axios({
+    method: 'POST',
+    url,
+    data
+  });
+}
+
+// get
+export function getAccountListApi(data) {
+  let url = accountApi.getAccountListApi;
+  return axios({
+    method: 'GET',
+    url,
+    data
+  });
+}
+
+// put修改账号
+export function putAccountApi(data) {
+  let url = accountApi.putAccountApi(data.accountUuid);
+  return axios({
+    method: 'PUT',
+    url,
+    data
+  });
+}
+
+// put修改密码
+export function putAccountPWDApi(data) {
+  let url = accountApi.putAccountPWDApi(data.accountName);
+  return axios({
+    method: 'PUT',
+    url,
+    data
+  });
+}
+
+// 重置密码
+export function resetAccountPWDApi(data) {
+  let url = accountApi.resetAccountPWDApi;
+  return axios({
+    method: 'PUT',
+    url,
+    data
+  });
+}
+
+// 删除账号
+export function deleteAccountApi(data) {
+  let url = accountApi.deleteAccountApi;
+  return axios({
+    method: 'DELETE',
     url,
     data
   });
