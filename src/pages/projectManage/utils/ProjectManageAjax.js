fb33a82a7d2b93d35284192205b82e730b0a6a83
KJ <kj@gato.com.cn>
2019-12-02 16:02:34 +0800

    eslint

--- src/pages/projectManage/utils/ProjectManageAjax.js
+++ src/pages/projectManage/utils/ProjectManageAjax.js
@@ -1,25 +1,26 @@
-import axios from '@/utils/Request';
-import RestApi from '@/utils/RestApi';
+import axios from "@/utils/Request";
+import RestApi from "@/utils/RestApi";
 
 let ip = window.config.ip;
 export var ProjectManageAjax = {
-
   /**
    * 登录
    */
   setLogUuid(holder) {
-    let api = `${window.location.protocol}//${ip}/${RestApi.api.login.setLogUuid}`;
+    let api = `${window.location.protocol}//${ip}/${
+      RestApi.api.login.setLogUuid
+    }`;
     return axios.post(api, `logUuid=${holder.logUuid}`, {
       headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
+        "Content-Type": "application/x-www-form-urlencoded"
         // "Authorization": "Basic d2ViOjEyMzQ1Ng=="
       }
     });
-  },
+  }
 };
 
 function install(Vue) {
   Vue.prototype.$ProjectManageAjax = ProjectManageAjax;
-};
+}
 
 export default install;
