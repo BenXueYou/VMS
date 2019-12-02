12d87a722fe956250e49a42655d0a967c831cda5
Your Name <you@example.com>
2019-12-02 16:46:01 +0800

    日志接口调试

--- src/pages/login/Login.vue
+++ src/pages/login/Login.vue
@@ -230,6 +230,14 @@ export default {
             "setProjectUuid",
             body.data.adminUser.projects[0].projectUuid
           );
+          this.$loginAjax
+	        .setLogUuid({
+	          logUuid: localStorage.getItem("logUuid")
+	        })
+	        .then(res => {
+	          // let body = res.data;
+	          // this.loginSuccessResponse(body);
+	        });
           // 以下是测试代码 projectUuid 299fc6dfa7104483bcee8d71e59957cd
           /** ***********测试代码*************** */
           this.$store.dispatch(
