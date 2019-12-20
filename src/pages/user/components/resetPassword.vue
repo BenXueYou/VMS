9d211bb3e357003dd06844cec4f7b94f6cbf543e
Your Name <you@example.com>
2019-12-20 11:46:44 +0800

    修改日志

--- src/pages/user/components/resetPassword.vue
+++ src/pages/user/components/resetPassword.vue
@@ -117,6 +117,23 @@ export default {
         this.$message.error("两次密码不同！");
         return;
       }
+      var eReg1 = /^(?![^a-zA-Z]+$)(?!\D+$)/;
+      if (!eReg1.test(this.password)) {
+        this.$message.warning("密码至少包含数字，字母这两种（区分大小写）");
+        return;
+      }
+      /* eslint-disable */
+      var eReg2 = /^[^\[\]\?\|\\\/\:\;\+\*\<\>]*$/;
+      /* eslint-enable */
+      if (!eReg2.test(this.password)) {
+        this.$message.warning("不能包含特殊字符 /[]:;丨+*?<>");
+        return;
+      }
+      var eReg3 = /^[^\s]*$/;
+      if (!eReg3.test(this.password)) {
+        this.$message.warning("字符中不能包含空格");
+        return;
+      }
       this.$emit("confirm", this.password);
       this.$emit("update:visible", false);
     },
