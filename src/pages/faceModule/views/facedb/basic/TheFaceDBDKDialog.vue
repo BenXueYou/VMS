79ab6c75c38274e908eadad2bfe0c790250ed272
pengxueyou <pxy@gato.com.cn>
2019-12-31 10:53:56 +0800

    更新人脸库的BUG

--- src/pages/faceModule/views/facedb/basic/TheFaceDBDKDialog.vue
+++ src/pages/faceModule/views/facedb/basic/TheFaceDBDKDialog.vue
@@ -1,59 +1,56 @@
 <template>
-  <div class="dialogwrap">
-    <el-dialog :title="title"
-               :visible.sync="diglogvisible"
-               :before-close="close"
-               width="580px"
-               center>
-      <el-form ref="form"
-               label-width="150px">
-        <el-form-item label="请选择模版类型：">
-          <el-select v-model="templatetype"
-                     collapse-tags
-                     placeholder="请选择"
-                     class="myinput">
-            <el-option v-for="item in templatetypearr"
-                       :key="item.typeStr"
-                       :label="item.typeName"
-                       :value="item.typeStr"></el-option>
-          </el-select>
-        </el-form-item>
+	<div class="dialogwrap">
+		<el-dialog
+			:title="title"
+			:visible.sync="diglogvisible"
+			:before-close="close"
+			width="580px"
+			center
+		>
+			<el-form ref="form" label-width="150px">
+				<el-form-item label="请选择模版类型：">
+					<el-select v-model="templatetype" collapse-tags placeholder="请选择" class="myinput">
+						<el-option
+							v-for="item in templatetypearr"
+							:key="item.typeStr"
+							:label="item.typeName"
+							:value="item.typeStr"
+						></el-option>
+					</el-select>
+				</el-form-item>
 
-        <div class="daorutips">
-          注：格式必须与模板一致
-          <span @click="downloadtempalte($event)"
-                style="cursor: pointer;">
-            <!-- <a id="alink"
-               download>模板下载</a> -->
-            模板下载
-          </span>
-        </div>
-        <el-form-item label="文件路径：">
-          <el-input class="buttoninput"
-                    :value="filename"
-                    readonly></el-input>
-          <el-button id="fileSelect"
-                     ref="fileSelect"
-                     type="info"
-                     style="height: 33.5px;margin-left: -61px;"
-                     size="small">浏览</el-button>
-          <!-- <el-input class="buttoninput"
+				<div class="daorutips">
+					注：格式必须与模板一致
+					<span @click="downloadtempalte($event)" style="cursor: pointer;">
+						<!-- <a id="alink"
+						download>模板下载</a>-->
+						模板下载
+					</span>
+				</div>
+				<el-form-item label="文件路径：">
+					<el-input class="buttoninput" :value="filename" readonly></el-input>
+					<el-button
+						id="fileSelect"
+						ref="fileSelect"
+						type="info"
+						style="height: 33.5px;margin-left: -61px;"
+						size="small"
+					>浏览</el-button>
+					<!-- <el-input class="buttoninput"
                     :value="filename"
                     readonly>
             <el-button id="fileSelect"
                        ref="fileSelect"
                        slot="append">浏览</el-button>
-          </el-input> -->
-        </el-form-item>
-      </el-form>
-      <span class="dialog-footer button-div btnBox">
-        <el-button type="primary"
-                   @click="onSubmit">确 定</el-button>
-        <el-button type="primary"
-                   @click="close">取 消</el-button>
-      </span>
-    </el-dialog>
-  </div>
+					</el-input>-->
+				</el-form-item>
+			</el-form>
+			<span class="dialog-footer button-div btnBox">
+				<el-button type="primary" :loading="onSubmitLoading" @click="onSubmit">确 定</el-button>
+				<el-button type="primary" @click="close">取 消</el-button>
+			</span>
+		</el-dialog>
+	</div>
 </template>
 
 <script>
@@ -83,6 +80,7 @@ export default {
   },
   data() {
     return {
+      onSubmitLoading: false,
       diglogvisible: false,
       templatetype: "static_person",
       templatetypearr: [],
@@ -140,9 +138,9 @@ export default {
           RestApi.api.faceModuleAPi.faceDBApi.downloadDBTemp(
             this.$store.state.home.projectUuid
           ) +
-            "?templateType=" +
-            this.templatetype,
-          this.templatetype === 'e_prisoner' ? "模板.txt" : "模板.xlsx",
+						"?templateType=" +
+						this.templatetype,
+          this.templatetype === "e_prisoner" ? "模板.txt" : "模板.xlsx",
           "GET"
         );
       }
@@ -162,6 +160,7 @@ export default {
           this.$store.state.home.projectUuid,
           this.libraryuuid
         );
+        this.onSubmitLoading = true;
         this.uploader.start();
       } else {
         this.$message.error("请上传文件！");
@@ -175,185 +174,186 @@ export default {
         return;
       }
       /* eslint-disable */
-      let num = new Date().getTime();
-      this.uploader = new plupload.Uploader({
-        browse_button: "fileSelect", // 这里填写触发元素的按钮ID
-        url: RestApi.api.faceModuleAPi.faceDBApi.addDaoKuTask(
-          _this.$store.state.home.projectUuid,
-          _this.libraryuuid
-        ), // 上传的地址
-        // url: "http://192.168.9.166:9220/v1/face/escaped/pluploadUpload",
-        headers: {
-          Authorization: _this.$store.state.home.Authorization
-        }, //token值
-        chunk_size: "5mb", // 分段上传，每个小段的大小
-        filters: {
-          max_file_size: "5000000mb"
-        },
-        multipart_params: {
-          templateType: _this.templatetype,
-          uuid: num
-        },
-        file_data_name: "multipartFile",
-        init: {
-          FileFiltered(up, file) {
-            console.log(file);
-          },
-          FilesAdded: function(up, files) {
-            for (var i = 0; i < _this.fileIdNum.length; i++) {
-              _this.uploader.removeFile(_this.fileIdNum[i].id);
-            }
-            _this.fileIdNum = [];
-            /* eslint-disable */
-            plupload.each(files, function(file) {
-              file.percent = 0;
-              _this.fileIdNum.push(file); // 这里在外面使用个全局的数组来记录上传了多少个文件然后进行处理操作。
-              // console.log(_this.fileIdNum);
-            });
-            alert("文件选择成功");
-          },
+			let num = new Date().getTime();
+			this.uploader = new plupload.Uploader({
+				browse_button: "fileSelect", // 这里填写触发元素的按钮ID
+				url: RestApi.api.faceModuleAPi.faceDBApi.addDaoKuTask(
+					_this.$store.state.home.projectUuid,
+					_this.libraryuuid
+				), // 上传的地址
+				// url: "http://192.168.9.166:9220/v1/face/escaped/pluploadUpload",
+				headers: {
+					Authorization: _this.$store.state.home.Authorization
+				}, //token值
+				chunk_size: "5mb", // 分段上传，每个小段的大小
+				filters: {
+					max_file_size: "5000000mb"
+				},
+				multipart_params: {
+					templateType: _this.templatetype,
+					uuid: num
+				},
+				file_data_name: "multipartFile",
+				init: {
+					FileFiltered(up, file) {
+						console.log(file);
+					},
+					FilesAdded: function(up, files) {
+						for (var i = 0; i < _this.fileIdNum.length; i++) {
+							_this.uploader.removeFile(_this.fileIdNum[i].id);
+						}
+						_this.fileIdNum = [];
+						/* eslint-disable */
+						plupload.each(files, function(file) {
+							file.percent = 0;
+							_this.fileIdNum.push(file); // 这里在外面使用个全局的数组来记录上传了多少个文件然后进行处理操作。
+							// console.log(_this.fileIdNum);
+						});
+						alert("文件选择成功");
+					},
 
-          /**
-           * 队列某个文件上传完成进行的回调
-           *  @param    uploader        为当前的plupload实例对象
-           *  @param    file            触发此事件的文件对象
-           *  @param    responseObject  服务器返回的信息对象
-           */
-          FileUploaded(uploader, file, responseObject) {
-            let res = JSON.parse(responseObject.response);
-            if (res.success) {
-              _this.$message.success("文件导入成功！");
-              _this.confirm();
-            } else {
-              _this.$message.error(res.msg);
-            }
-          },
-          /**
-           * 所有文件上传完成回调函数
-           * @param   uploader  当前uploader的实例对象
-           * @param   files     上传完成的所有文件对象
-           */
-          UploadComplete(uploader, files) {},
-          Error: function(up, err) {
-            console.log("\nError #" + err.code + ": " + err.message);
-          }
-        }
-      });
-      this.uploader.init();
-    },
-    clearallData() {
-      this.templatetype = "";
-      this.fileIdNum = [];
-    },
-    close() {
-      this.clearallData();
-      this.$emit("close");
-    },
-    confirm() {
-      this.clearallData();
-      this.$emit("confirm");
-    }
-  }
+					/**
+					 * 队列某个文件上传完成进行的回调
+					 *  @param    uploader        为当前的plupload实例对象
+					 *  @param    file            触发此事件的文件对象
+					 *  @param    responseObject  服务器返回的信息对象
+					 */
+					FileUploaded(uploader, file, responseObject) {
+						let res = JSON.parse(responseObject.response);
+						this.onSubmitLoading = false;
+						if (res.success) {
+							_this.$message.success("文件导入成功！");
+							_this.confirm();
+						} else {
+							_this.$message.error(res.msg);
+						}
+					},
+					/**
+					 * 所有文件上传完成回调函数
+					 * @param   uploader  当前uploader的实例对象
+					 * @param   files     上传完成的所有文件对象
+					 */
+					UploadComplete(uploader, files) {},
+					Error: function(up, err) {
+						console.log("\nError #" + err.code + ": " + err.message);
+					}
+				}
+			});
+			this.uploader.init();
+		},
+		clearallData() {
+			this.templatetype = "";
+			this.fileIdNum = [];
+		},
+		close() {
+			this.clearallData();
+			this.$emit("close");
+		},
+		confirm() {
+			this.clearallData();
+			this.$emit("confirm");
+		}
+	}
 };
 </script>
 <style lang='scss' scoped>
 .color {
-  display: inline-block;
-  width: 16px;
-  height: 16px;
-  vertical-align: middle;
-  border-radius: 2px;
-  background-color: #fff;
+	display: inline-block;
+	width: 16px;
+	height: 16px;
+	vertical-align: middle;
+	border-radius: 2px;
+	background-color: #fff;
 }
 .btn-center {
-  position: absolute;
-  left: 50%;
-  height: 50px;
-  transform: translateX(-50%);
+	position: absolute;
+	left: 50%;
+	height: 50px;
+	transform: translateX(-50%);
 }
 .myinput {
-  width: 260px;
+	width: 260px;
 }
 .daorutips {
-  margin: 10px 0px 20px 150px;
-  font-family: "PingFangSC-Regular";
-  font-size: 12px;
-  color: #bbbbbb;
-  span {
-    color: #28ffbb;
-    cursor: pointer;
-    a {
-      color: #28ffbb;
-    }
-  }
+	margin: 10px 0px 20px 150px;
+	font-family: "PingFangSC-Regular";
+	font-size: 12px;
+	color: #bbbbbb;
+	span {
+		color: #28ffbb;
+		cursor: pointer;
+		a {
+			color: #28ffbb;
+		}
+	}
 }
 .customize {
-  border-top: 1px dashed #666;
-  border-bottom: 1px dashed #666;
-  height: 150px;
-  overflow: hidden;
-  .wrap {
-    width: 555px;
-    margin: 0 auto;
-    padding: 20px 0px;
-    line-height: 50px;
-    font-size: 0px;
-    .itemlabel {
-      font-family: "PingFangSC-Regular";
-      display: inline-block;
-      width: 120px;
-      font-size: 14px;
-      text-align: right;
-      color: #aaaaaa;
-    }
-    .itemInput {
-      display: inline-block;
-      .el-input {
-        width: 65px;
-      }
-    }
-  }
+	border-top: 1px dashed #666;
+	border-bottom: 1px dashed #666;
+	height: 150px;
+	overflow: hidden;
+	.wrap {
+		width: 555px;
+		margin: 0 auto;
+		padding: 20px 0px;
+		line-height: 50px;
+		font-size: 0px;
+		.itemlabel {
+			font-family: "PingFangSC-Regular";
+			display: inline-block;
+			width: 120px;
+			font-size: 14px;
+			text-align: right;
+			color: #aaaaaa;
+		}
+		.itemInput {
+			display: inline-block;
+			.el-input {
+				width: 65px;
+			}
+		}
+	}
 }
 </style>
 
 <style lang='scss'>
 // 下拉上弹动画
 .slideDownOrUp-enter-active {
-  transition: all 0.3s;
+	transition: all 0.3s;
 }
 .slideDownOrUp-enter {
-  transform: scale(1, 0);
+	transform: scale(1, 0);
 }
 .slideDownOrUp-leave-to /* .fade-leave-active below version 2.1.8 */ {
-  transform: scale(1, 1);
+	transform: scale(1, 1);
 }
 
 /* 本页面自定义 */
 .dialogwrap {
-  .el-form-item {
-    margin-bottom: 10px;
-    .buttoninput {
-      width: 320px;
-    }
-  }
-  .el-input__inner {
-    padding: 0px 5px 0px 8px;
-    border: 1px solid rgba($color: #ffffff, $alpha: 0.15);
-    border-radius: 2px;
-  }
-  .el-form-item__label {
-    font-family: "PingFangSC-Regular";
-    display: inline-block;
-    width: 120px;
-    font-size: 14px;
-    text-align: right;
-    color: #dddddd;
-  }
-  .btnBox {
-    width: 200px;
-    display: block;
-    overflow: hidden;
-    margin: 20px auto;
-  }
+	.el-form-item {
+		margin-bottom: 10px;
+		.buttoninput {
+			width: 320px;
+		}
+	}
+	.el-input__inner {
+		padding: 0px 5px 0px 8px;
+		border: 1px solid rgba($color: #ffffff, $alpha: 0.15);
+		border-radius: 2px;
+	}
+	.el-form-item__label {
+		font-family: "PingFangSC-Regular";
+		display: inline-block;
+		width: 120px;
+		font-size: 14px;
+		text-align: right;
+		color: #dddddd;
+	}
+	.btnBox {
+		width: 200px;
+		display: block;
+		overflow: hidden;
+		margin: 20px auto;
+	}
 }
 </style>
