e924d94efaa440b59dc51219226b147668fe7ac0
Your Name <you@example.com>
2020-02-13 16:28:15 +0800

    调试接口

--- src/pages/user/components/accountAdd.vue
+++ src/pages/user/components/accountAdd.vue
@@ -411,7 +411,7 @@ export default {
       this.queryBody.staffUuid = arr[0].id;
       this.queryBody.staffName = arr[0].label;
       // if(arr[0].contact[0].contactType) {
-        
+
       // }
       this.queryBody.phoneNumber = arr[0].cellphone;// 电话号码
       this.queryBody.emailNumber = arr[0].email; // 邮箱号码
