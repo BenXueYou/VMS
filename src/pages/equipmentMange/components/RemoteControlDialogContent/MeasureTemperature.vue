e83ba12b0e4066d285fe50547e27f9928a5aa37e
KJ <kj@gato.com.cn>
2020-03-09 10:41:11 +0800

    修改bug

--- src/pages/equipmentMange/components/RemoteControlDialogContent/MeasureTemperature.vue
+++ src/pages/equipmentMange/components/RemoteControlDialogContent/MeasureTemperature.vue
@@ -17,7 +17,8 @@
           </el-switch>
         </el-form-item>
 
-        <el-form-item label="高温报警上传：">
+        <el-form-item label="高温报警上传："
+                      v-show="data.tempMeasureEnable">
           <el-switch v-model="data.highTempAlarmUploadEnable"
                      active-color="rgb(38,78,70)"
                      inactive-color="rgb(75,78,82)">
@@ -25,6 +26,7 @@
         </el-form-item>
 
         <el-form-item label="高温阈值："
+                      v-show="data.tempMeasureEnable"
                       prop="highTemperatureThrold">
           <el-input v-model='data.highTemperatureThrold'
                     type="number">
@@ -34,21 +36,23 @@
           </span>
         </el-form-item>
 
-        <el-form-item label="显示测温结果：">
+        <el-form-item label="显示测温结果："
+                      v-show="data.tempMeasureEnable">
           <el-switch v-model="data.showTempMeasureEnable"
                      active-color="rgb(38,78,70)"
                      inactive-color="rgb(75,78,82)">
           </el-switch>
         </el-form-item>
 
-        <el-form-item label="播报测温结果：">
+        <el-form-item label="播报测温结果："
+                      v-show="data.tempMeasureEnable">
           <el-switch v-model="data.broadcastTempMeasureEnable"
                      active-color="rgb(38,78,70)"
                      inactive-color="rgb(75,78,82)">
           </el-switch>
         </el-form-item>
 
-        <el-form-item label="测温参数：">
+        <!-- <el-form-item label="测温参数：">
           <span class="operator"
                 @click="opening=true"
                 v-if="!opening">展开<img src="@/assets/temperature/slideup.png"
@@ -57,7 +61,7 @@
                 @click="opening=false"
                 v-else>收起<img src="@/assets/temperature/slidedown.png"
                  alt=""></span>
-        </el-form-item>
+        </el-form-item> -->
 
         <div v-show="opening">
           <el-form-item label="目标距离："
